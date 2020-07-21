246a8ca037196e988206fb567f396ae75e464b7d
David Tennander <david.tennander@gmail.com>
2020-07-21 15:31:09 +0200

    Refactors Data Query path.

--- src/API/Countries.elm
+++ src/API/Countries.elm
@@ -1,48 +1,81 @@
-module API.Countries exposing (parseCountryCode, getCountryName, getGDP, CountryCode, Time(..), getRuralPop)
+module API.Countries exposing (
+    Country,
+    parseCountryCode,
+    iso2Code,
+
+    Time(..),
+    getCountryName,
+    getCountryGDP,
+    getCountryRuralPop
+ , getSchoolEnrolment, getCO2PerCapita)
 
 import Http
-import Json.Decode as Decode exposing (Decoder, field, float, index, list, string)
+import Json.Decode as Decode exposing (Decoder, field, float, index, list, maybe, string)
 import String exposing (toInt)
 
-type CountryCode = CountryCode Char Char
+{-| Represents one country on Earth.
+Is created by using parseCountryCode.
+-}
+type Country = CountryCode Char Char
 
-parseCountryCode : String -> Maybe CountryCode
+{-|Parses a string as an ISO2 country code.
+Returns a Country if it is a valid code and Nothing otherwise.
+-}
+parseCountryCode : String -> Maybe Country
 parseCountryCode = String.toList >> \l -> case l of
     [a,b] -> Just <| CountryCode a b
     _ -> Nothing
 
-iso2Code : CountryCode -> String
+{-| Generates the ISO 2 Country code for a given country.
+-}
+iso2Code : Country -> String
 iso2Code (CountryCode a b) = String.fromList [a,b]
 
-type alias Getter r m = Time -> (Result Http.Error r -> m) -> CountryCode -> Cmd m
-
-getCountryName : (Result Http.Error String -> m) -> CountryCode -> Cmd m
+{-| Command that queries the World Bank for the full name of a Country.
+    Takes a Message constructor that wll construct the sent message.
+-}
+getCountryName : (Result Http.Error String -> m) -> Country -> Cmd m
 getCountryName toMsg cc = Http.get {
         url = "http://api.worldbank.org/v2/country/" ++ (iso2Code cc) ++ "?format=json",
         expect = Http.expectJson toMsg
             (index 1 <| index 0 <| field "name" string)
     }
 
-type alias Indicator = String
 
-type alias Year = Int
+{-| Represents a time span for which to get the resulting data -}
 type Time = Latest | Exact Year | Range Year Year
 
-getIndicator : Indicator -> Getter (List (Year, Float)) m
+{-| Returns a list of the Country's GDP for each year in the given Time span. -}
+getCountryGDP : Time -> (Result Http.Error (List (Year, Float)) -> m) -> Country -> Cmd m
+getCountryGDP = getIndicator "NY.GDP.MKTP.CD"
+{-| Returns a list of the Country's Ruraö population for each year in the given Time span. -}
+getCountryRuralPop : Time -> (Result Http.Error (List (Year, Float)) -> m) -> Country -> Cmd m
+getCountryRuralPop = getIndicator "SP.RUR.TOTL.ZS"
+
+getSchoolEnrolment : Time -> (Result Http.Error (List (Year, Float)) -> m) -> Country -> Cmd m
+getSchoolEnrolment = getIndicator "SE.PRM.ENRR"
+getCO2PerCapita : Time -> (Result Http.Error (List (Year, Float)) -> m) -> Country -> Cmd m
+getCO2PerCapita = getIndicator "EN.ATM.CO2E.PC"
+
+
+type alias Indicator = String
+type alias Year = Int
+
+getIndicator : Indicator -> Time -> (Result Http.Error (List (Year, Float)) -> m) -> Country -> Cmd m
 getIndicator indicator t toMsg cc =
     let
         decodeYear = field "date" string |> Decode.map (toInt >> Maybe.withDefault 0)
+        gatherValues = List.foldr (\v l ->
+            case v of
+                (y, Just val) -> (y, val) :: l
+                (_, Nothing) -> l
+            ) []
         filter = case t of
-            Latest -> List.sortBy (\(year,_) -> year) >> List.reverse >> List.take 1
-            Exact year -> List.filter (\(y,_) -> y == year)
-            Range from to -> List.filter (\(y,_) -> from <= y && y <= to)
+            Latest -> List.sortBy (\(year,_) -> year) >> List.reverse >> gatherValues >> List.take 1
+            Exact year -> List.filter (\(y,_) -> y == year) >> gatherValues
+            Range from to -> List.filter (\(y,_) -> from <= y && y <= to) >> gatherValues
     in Http.get {
          url = "http://api.worldbank.org/v2/country/" ++ (iso2Code cc) ++ "/indicator/"++ indicator ++"?format=json",
          expect = Http.expectJson (Result.map filter >> toMsg)
-             (index 1 <| list <| Decode.map2 (\a b -> (a, b)) decodeYear (field "value" float))
+             (index 1 <| list <| Decode.map2 (\a b -> (a, b)) decodeYear (maybe <| field "value" float))
      }
-
-
-getGDP = getIndicator "NY.GDP.MKTP.CD"
-getMiddleHeadCount = getIndicator "1.0.HCount.Mid10to50"
-getRuralPop = getIndicator "SP.RUR.TOTL.ZS"
--- src/Page/Question.elm
+++ src/Page/Question.elm
@@ -1,72 +1,104 @@
 module Page.Question exposing (view, Model, init, update, Msg)
 
-import API.Countries exposing (CountryCode, Time(..), getCountryName, getGDP, getRuralPop, parseCountryCode)
-import Debug exposing (toString)
+import API.Countries exposing (Country, Time(..), getCO2PerCapita, getCountryGDP, getCountryName, getCountryRuralPop, getSchoolEnrolment, parseCountryCode)
+import Debug
 import Html exposing (..)
 import Http
 import String exposing (fromFloat, fromInt)
 
 -- MODEL
-type alias Model = {
-        code: Maybe CountryCode,
+type Model = Valid Country Data | Invalid
+
+type alias Data = {
         name: Maybe String,
-        gdp: Maybe (Year, Float),
-        electricity: Maybe (Year, Float),
-        lastError: Maybe Http.Error
+        ruralPopulation: Maybe (Year, Float),
+        schoolEnrolment: Maybe (Year, Float),
+        co2PerCapita: Maybe (Year, Float),
+        lastFetchError: Maybe Http.Error
     }
 
 init: String -> (Model, Cmd Msg)
 init cc =
-    let baseModel = {name = Nothing, gdp = Nothing, code = Nothing, lastError = Nothing, electricity = Nothing}
+    let baseModel = {
+            name = Nothing,
+            lastFetchError = Nothing,
+            ruralPopulation = Nothing,
+            schoolEnrolment = Nothing,
+            co2PerCapita = Nothing
+            }
     in case parseCountryCode cc of
-        Just c -> ({baseModel | code = Just c}, Cmd.batch [fetchCountryName c, fetchCountryGDP c, fetchElectricity c])
-        Nothing -> (baseModel, Cmd.none)
+        Just c -> (Valid c baseModel, Cmd.batch [
+                fetchName c,
+                fetchRuralPopulation c,
+                fetchSchoolEnrolment c,
+                fetchCo2 c
+            ])
+        Nothing -> (Invalid, Cmd.none)
 
 
 -- UPDATE
 type Msg =
       FailedToFetch Http.Error
-    | FetchedName String
-    | FetchedGDP Year Float
-    | FetchedElectricity Year Float
+    | FetchedDatapoint Datapoint
+
+type Datapoint =
+      Name String
+    | RuralPopulation Year Float
+    | SchoolEnrolment Year Float
+    | CO2PerCapita Year Float
 
 type alias Year = Int
 
 update: Msg -> Model -> (Model, Cmd Msg)
-update msg model = case msg of
-    FetchedName n -> ({model| name= Just n}, Cmd.none)
-    FetchedGDP y gdp -> ({model| gdp= Just (y, gdp)}, Cmd.none)
-    FetchedElectricity y percent -> ({model| electricity= Just (y, percent)}, Cmd.none)
-    FailedToFetch e -> ({model | lastError = Just (Debug.log "error" e)}, Cmd.none)
+update msg model = case (model, msg) of
+    (Valid c d, FetchedDatapoint dp) ->
+        (Valid c (updateData dp d), Cmd.none)
+    (Valid c m, FailedToFetch e) ->
+        (Valid c {m | lastFetchError = Just (Debug.log "error" e)}, Cmd.none)
+    (Invalid, _) -> (model, Cmd.none)
+
+updateData : Datapoint -> Data -> Data
+updateData dp d = case dp of
+    Name n -> {d | name = Just n}
+    RuralPopulation y v -> {d | ruralPopulation = Just (y,v)}
+    SchoolEnrolment y v -> {d | schoolEnrolment = Just (y,v)}
+    CO2PerCapita y v -> {d | co2PerCapita = Just (y,v)}
+
 -- VIEW
 
-unknown : Maybe String -> String
-unknown = Maybe.withDefault "???"
-
 view: Model -> Html Msg
-view model = div [] [
-    h1 [] [text (model.name |> unknown)],
-    h2 [] [text ("GDP: " ++ (model.gdp |> Maybe.map (\(y,v) -> fromFloat v ++ " (" ++ fromInt y ++ ")")|> unknown))],
-    h2 [] [text ("Rural Population: " ++ (model.electricity |> Maybe.map (\(y,v) -> fromFloat v ++ "% (" ++ fromInt y ++ ")")|> unknown))]
- ]
+view m = case m of
+    Valid _ d -> viewValid d
+    Invalid -> h1 [] [text "No country could be parsed from the given Country Code"]
+
+viewValid: Data -> Html Msg
+viewValid d =
+    let
+        toString (y, v) = fromFloat v ++ " (" ++ fromInt y ++ ")"
+        stat txt value = h2 [] [
+                text (txt ++ ": " ++ (value |> Maybe.map toString |> Maybe.withDefault "???"))
+            ]
+    in div [] [
+        h1 [] [text "Is this a Dictatorship or Democracy?"],
+        stat "Rural Population [%]" d.ruralPopulation,
+        stat "School Enrolment [%]" d.schoolEnrolment,
+        stat "CO2 releases per Capita [Ton]" d.co2PerCapita
+    ]
 
 -- ACTIONS
-fetchCountryName: CountryCode -> Cmd Msg
-fetchCountryName = getCountryName <|
+fetchName = getCountryName <|
     \r -> case r of
-        Ok name -> FetchedName name
+        Ok name -> FetchedDatapoint (Name name)
         Err e -> FailedToFetch e
+fetchRuralPopulation = fetchWith getCountryRuralPop RuralPopulation
+fetchSchoolEnrolment = fetchWith getSchoolEnrolment SchoolEnrolment
+fetchCo2 = fetchWith getCO2PerCapita CO2PerCapita
 
-fetchCountryGDP: CountryCode -> Cmd Msg
-fetchCountryGDP = getGDP Latest <|
-    \r -> case r of
-        Ok [(year, gdp)] -> FetchedGDP year gdp
-        Err e -> FailedToFetch e
-        _ -> FailedToFetch <| Http.BadBody "Got to many matches..."
+type alias Fetcher = Time -> (Result Http.Error (List (Year, Float)) -> Msg) -> Country -> Cmd Msg
 
-fetchElectricity: CountryCode -> Cmd Msg
-fetchElectricity = getRuralPop Latest <|
+fetchWith: Fetcher -> (Year -> Float -> Datapoint) -> Country -> Cmd Msg
+fetchWith fetcher toDataPoint = fetcher Latest <|
     \r -> case r of
-        Ok [(year, gdp)] -> FetchedElectricity year gdp
-        Err e -> FailedToFetch e
-        _ -> FailedToFetch <| Http.BadBody "Got to many matches..."
+       Ok [(year, gdp)] -> FetchedDatapoint (toDataPoint year gdp)
+       Err e -> FailedToFetch e
+       _ -> FailedToFetch <| Http.BadBody "Got to many matches..."
